5ee3325e907923f21405d91afd8f4c24f973d8d1
Ville L <villelohkovuori1@gmail.com>
2018-01-04 22:55:06 +0200

    setting things up to work...

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('HEY!')
+})
+
+app.listen(3000, () => console.log('Server running on port 3000'))
+
+
+
+
+
+
+
